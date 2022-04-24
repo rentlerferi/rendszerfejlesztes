284a26a45a71a22dc4c7879e8055ec0c7bec775d
marcellkovacs <79199376+Kowwi@users.noreply.github.com>
2022-04-24 19:16:20 +0200

    Summary Comit

--- app/src/main/java/com/example/api/EmergencyTasks.java
+++ app/src/main/java/com/example/api/EmergencyTasks.java
@@ -1,8 +1,5 @@
 package com.example.api;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -13,14 +10,20 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.nio.BufferUnderflowException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class EmergencyTasks extends AppCompatActivity {
@@ -32,43 +35,51 @@ public class EmergencyTasks extends AppCompatActivity {
     ArrayList<String> all_tools;
     Button BuTtOn1;
 
+    String idx = "";
+
     FirebaseDatabase fb;
-    DatabaseReference tool_ref,task_ref;
+    DatabaseReference tool_ref, task_ref;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency_tasks);
 
-        BuTtOn1=findViewById(R.id.commitBttn);
-        list=findViewById(R.id.itemList);
-        instructions=findViewById(R.id.instructions);
-        date=findViewById(R.id.date);
-        location=findViewById(R.id.Location);
+        BuTtOn1 = findViewById(R.id.commitBttn);
+        list = findViewById(R.id.itemList);
+        instructions = findViewById(R.id.instructions);
+        date = findViewById(R.id.date);
+        location = findViewById(R.id.Location);
 
-        fb= FirebaseDatabase.getInstance(getResources().getString(R.string.database_url));
-        tool_ref=fb.getReference("Tool Categories");
-        task_ref=fb.getReference("Task");
+        fb = FirebaseDatabase.getInstance(getResources().getString(R.string.database_url));
+        tool_ref = fb.getReference("Tool Categories");
+        task_ref = fb.getReference("Task");
 
         all_tools = new ArrayList<>();
         ArrayAdapter<String> tool_adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, all_tools);
         list.setAdapter(tool_adapter);
-        HashMap<String,String> location_hess = new HashMap<>();
+        HashMap<String, Tool> tools_hess  = new HashMap<>();
+
+        HashMap<String, String > fcingthing  = new HashMap<>();
 
         tool_ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 all_tools.clear();
-                location_hess.clear();
+                tools_hess.clear();
                 for (DataSnapshot item : snapshot.getChildren()) {
-                    DatabaseReference temp=tool_ref.child(item.getKey()).child("Tools");
+                    DatabaseReference temp = tool_ref.child(item.getKey()).child("Tools");
+                    fcingthing.put(item.getKey(), item.getValue(Category.class).getInterval());
                     temp.addValueEventListener(new ValueEventListener() {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot1) {
-                            for (DataSnapshot tool:snapshot1.getChildren()) {
+                            for (DataSnapshot tool : snapshot1.getChildren()) {
                                 all_tools.add(tool.getKey());
-                                location_hess.put(tool.getKey(),tool.getValue(Tool.class).getLocation());
+                                tools_hess.put(tool.getKey(), tool.getValue(Tool.class));
+
                             }
+                            tool_adapter.notifyDataSetChanged();
                         }
 
                         @Override
@@ -78,7 +89,7 @@ public class EmergencyTasks extends AppCompatActivity {
                     });
 
                 }
-                tool_adapter.notifyDataSetChanged();
+
             }
 
             @Override
@@ -91,8 +102,9 @@ public class EmergencyTasks extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
-                String idy = list.getItemAtPosition(i).toString();
-                location.setText("Location ->  "+location_hess.get(idy));
+                idx= list.getItemAtPosition(i).toString();
+
+                location.setText("Location ->  " + tools_hess.get(idx).getLocation());
 
             }
 
@@ -103,11 +115,14 @@ public class EmergencyTasks extends AppCompatActivity {
         });
 
 
-
         BuTtOn1.setOnClickListener(view -> {
-
+            SimpleDateFormat simpleDateFormatter = new SimpleDateFormat("yyyy-MM-dd");
+            String date_date = simpleDateFormatter.format(date.getDate()).toString();
+            Tool aTool = tools_hess.get(idx);
+            Task task=new Task(aTool.getName(),aTool.getLocation(),instructions.getText().toString(),date_date,fcingthing.get(aTool.getCategory()),true);
+            task_ref.push().setValue(task);
+            
         });
-
 
 
     }
