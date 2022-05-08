17f7c1d5b0400a8103ae8676208c1ed93e722a2d
Orsós Emil István <62039212+SnarkyGoblin092@users.noreply.github.com>
2022-05-08 12:42:52 +0200

    .

---
+++ app/src/main/java/com/example/api/AddProfession.java
@@ -0,0 +1,131 @@
+package com.example.api;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class AddProfession extends AppCompatActivity {
+
+    EditText knowledge;
+    Button add, delete;
+    Spinner usersSpinner, professionSpinner;
+
+    User user;
+    String profession;
+
+    ArrayList<String> userIDs = new ArrayList<>();
+    ArrayList<String> userProfessions = new ArrayList<>();
+
+    ArrayAdapter<String> userAdapter, profAdapter;
+
+    DatabaseReference ref, userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_profession);
+
+        usersSpinner = findViewById(R.id.usersSpinner);
+        professionSpinner = findViewById(R.id.professionSpinner);
+        knowledge = findViewById(R.id.knowledge);
+        add = findViewById(R.id.addProfession);
+        delete = findViewById(R.id.delProfession);
+
+        ref = FirebaseDatabase.getInstance(getResources().getString(R.string.database_url)).getReference("Users");
+
+        userAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, userIDs);
+        profAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, userProfessions);
+        usersSpinner.setAdapter(userAdapter);
+        professionSpinner.setAdapter(profAdapter);
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userIDs.clear();
+                for (DataSnapshot item: snapshot.getChildren()) {
+                    userIDs.add(item.getKey());
+                }
+                userAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        usersSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String id = usersSpinner.getSelectedItem().toString();
+                userRef = ref.child(id);
+                updateProfessions();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+        add.setOnClickListener(view -> {
+            profession = knowledge.getText().toString();
+            if (userProfessions.contains(profession)) return;
+            user.addProfession(profession);
+            userRef.setValue(user);
+            updateProfessions();
+        });
+
+        delete.setOnClickListener(view -> {
+            profession = professionSpinner.getSelectedItem().toString();
+            user.removeProfession(profession);
+            userRef.setValue(user);
+            updateProfessions();
+        });
+    }
+
+    private void updateProfessions(){
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @SuppressLint("NewApi")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                user = snapshot.getValue(User.class);
+                userProfessions.clear();
+                userProfessions.addAll(user.getProfessions());
+                profAdapter.notifyDataSetChanged();
+
+                if (userProfessions.isEmpty()){
+                    professionSpinner.setEnabled(false);
+                    delete.setEnabled(false);
+                }
+                else {
+                    professionSpinner.setEnabled(true);
+                    delete.setEnabled(true);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/api/ToolCorrespondentCertain.java
@@ -0,0 +1,94 @@
+package com.example.api;
+
+import static com.example.api.Helpers.getTime;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ToolCorrespondentCertain extends AppCompatActivity {
+
+    Button addTool;
+    EditText toolName, toolLocation, toolDescription, taskInstruction;
+    Spinner toolCategory;
+    HashMap<String,Category>  categories = new HashMap<>();
+    ArrayList<String> categoryNames = new ArrayList<>();
+
+    String tool_name, tool_location, tool_category, tool_description, task_instruction;
+
+    DatabaseReference ref;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tool_correspondent_certain);
+
+        toolCategory = findViewById(R.id.toolCategory);
+        addTool = findViewById(R.id.addTool);
+        toolName = findViewById(R.id.toolName);
+        toolLocation = findViewById(R.id.toolLocation);
+        toolDescription = findViewById(R.id.toolDescription);
+        taskInstruction = findViewById(R.id.taskInstruction);
+        ref = FirebaseDatabase.getInstance(getResources().getString(R.string.database_url)).getReference();
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, categoryNames);
+        toolCategory.setAdapter(adapter);
+
+        ref.child("Tool Categories").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                categories.clear();
+                categoryNames.clear();
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Category category = snapshot.getValue(Category.class);
+                    categories.put(snapshot.getKey(),category);
+                    categoryNames.add(snapshot.getKey());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("TAG", "loadPost:onCancelled", error.toException());
+            }
+        });
+
+        addTool.setOnClickListener(view -> {
+            if (!toolName.getText().toString().equals("") &&
+                    !toolLocation.getText().toString().equals("")) {
+                tool_name = toolName.getText().toString();
+                tool_location = toolLocation.getText().toString();
+                tool_category = toolCategory.getSelectedItem().toString();
+                tool_description = toolDescription.getText().toString();
+                task_instruction = taskInstruction.getText().toString();
+
+                Tool tool = new Tool(tool_name, tool_location, tool_category, tool_description);
+                Task task = new Task(tool_name, tool_location, task_instruction, getTime(categories.get(tool_category).interval), "Unassigned", categories.get(tool_category).interval);
+
+                DatabaseReference toolRef = ref.child("Tool Categories").child(tool_category).child("Tools");
+                DatabaseReference taskRef = ref.child("Tasks").child(tool_name);
+
+                toolRef.child(tool_name).setValue(tool);
+                taskRef.setValue(task);
+            } else {
+                Toast.makeText(ToolCorrespondentCertain.this, "Fill all the required fields!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
